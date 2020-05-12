10da12e49693976acb537a2f0824828891e81b49
Michael Bade <mbade314@gmail.com>
2020-05-12 15:20:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,16 @@
 def reverse_each_word(string)
   arr = string.split(" ")
-  arr.collect do |var|
-    var.reverse
-    end
-  arr.join(" ")
-end
-
-def reverse_each_word(string)
-  arr = string.split(" ")
   reversed_strings = []
   arr.each do |var|
     reversed_strings << var.reverse
     end
     reversed_strings.join(" ")
+end
+
+def reverse_each_word(string)
+  arr = string.split(" ")
+  arr.collect do |var|
+    var.reverse
+    end
+  arr.join(" ")
 end
